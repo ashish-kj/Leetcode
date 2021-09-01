2680368efe110e9c2663dda11980d1ee8f3221cf
Ashish-BITS <you@example.com>
2021-09-02 00:40:37 +0400

    DSA

---
+++ 01092021_DSA_1.py
@@ -0,0 +1,20 @@
+#input 
+nums = list(map(int, input().split()))
+target = int(input())
+
+#variables
+ret = list()
+temp = 0
+l = len(nums)
+
+#Algo
+
+for i in range(l):
+    for j in range(i+1,l):
+        temp = nums[i] + nums[j]
+        if(temp == target):
+            ret.append(i)
+            ret.append(j)
+            break
+
+print(ret)
